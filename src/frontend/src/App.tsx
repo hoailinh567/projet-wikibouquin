75372572b2257c8997508a03118e2103c618419e
NGUYEN Hoai Linh <hoai-linh.nguyen@oclock.school>
2025-10-27 12:48:38 +0100

    front: import "BrowserRouter" and prepare the route for calling APIs

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,13 +1,16 @@
-import BookDetail from "./components/bookDetail";
+import { useParams } from "react-router";
+import BookDetails from "./components/book_details";
 import Footer from "./components/footer";
 import Header from "./components/header";
 
 function App() {
+  let { isbn } = useParams();
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="grow">
-        <BookDetail />
+        <BookDetails isbn={isbn} />
       </main>
       <Footer />
     </div>
--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from "react-router";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/book/:isbn" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
