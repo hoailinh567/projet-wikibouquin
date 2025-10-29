bc504914d7479070b8dd2f4a671f9a0a178f4af6
NGUYEN Hoai Linh <hoai-linh.nguyen@oclock.school>
2025-10-29 01:18:53 +0100

    back: new Fetch to get the description of book.

--- src/backend/src/client/openlibrary.ts
+++ src/backend/src/client/openlibrary.ts
@@ -19,7 +19,18 @@ const openlibraryClient = {
     }
     const data = await response.json();
     return data;
-  }
+  },
+
+  async getWorkByKey(workKey: string) {
+    const response = await fetch(`https://openlibrary.org/works/${workKey}.json`, {
+      headers: { "User-Agent": "Wikibouquin/1.0 (hoai-linh.nguyen@oclock.school)" },
+    });
+    if (!response.ok) {
+      throw new Error(`Work non trouvée pour la clé: ${workKey}`);
+    }
+    const data = await response.json();
+    return data;
+  },
 };
 
 export default openlibraryClient;
