d412ec8037fcd2a93a8bde284b4ae70a93a330f6
NGUYEN Hoai Linh <hoai-linh.nguyen@oclock.school>
2025-11-04 01:51:54 +0100

    back: prepare the routes for collection

--- src/backend/src/router.ts
+++ src/backend/src/router.ts
@@ -3,15 +3,27 @@ import bookController from "./controller/book.controller.ts";
 import authController from "./controller/auth.controller.ts";
 import authenticate from "./middleware/authenticate.ts";
 import profileController from "./controller/profile.controller.ts";
+import collectionController from "./controller/collection.controller.ts";
 
 const router = Router();
 
-router.get('/api/book/:isbn', bookController.getBookByIsbn);
+// Auth
 router.post('/api/signup', authController.signUp);
 router.post('/api/signin', authController.signIn);
 router.post('/api/refresh', authController.refresh);
 router.post('/api/logout', authController.logout);
+
+// App
+router.get('/api/book/:isbn', bookController.getBookByIsbn);
+
+// User
 router.get('/api/my-profile', authenticate, profileController.getMyProfile);
+
+// Collection
+router.get('/api/my-collection', authenticate, collectionController.manageMyCollection);
+router.post('/api/my-collection/add', authenticate, collectionController.addBook);
+router.delete('/api/my-collection/delete', authenticate, collectionController.deleteBook);
+router.patch('/api/my-collection/update-visibility', authenticate, collectionController.updateVisibility)
 
 
 export default router;
